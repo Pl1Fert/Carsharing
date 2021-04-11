d1921b9fadfbf1f4ef8d11f1f7588a10ceeb0d9c
PL1Fert <alexshashok2003@gmail.com>
2021-04-11 23:24:22 +0300

    learnt slider

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let slide_index = 1;
+show_slides(slide_index);
+
+function currentSlide(n){
+    show_slides(slide_index = n);
+}
+
+function show_slides(index){
+    let slide = document.getElementsByClassName("top__slider-item");
+    let dot = document.getElementsByClassName("dot");
+
+    if(index > slide.length) slide_index = 1;
+    if(index < 1) slide_index = slide.length;
+
+    for(let i = 0;i < slide.length ; i++){
+        slide[i].style.display = "none";
+        //slide[i].hide();
+    }
+
+    for(let i = 0;i < slide.length ; i++){
+        dot[i].classList.remove("active");
+
+    }
+
+    slide[slide_index-1].style.display = "block";
+    dot[slide_index - 1].classList.add("active");
+}
+
+let timer = 0;
+makeTimer(); //Создаем интервал 
+function makeTimer(){
+    clearInterval(timer); //Очистим интервал, это позволит прервать его работу и отменить перелистывание
+    timer = setInterval(function(){
+    slide_index++;
+    show_slides(slide_index);
+    },2000);
+}
